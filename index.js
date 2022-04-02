d634981e7c869f714a1584da3858f613a6692938
atherashraf <athear.ashraf@mic3.com>
2022-04-02 17:13:05 +0300

    WMS IN GOOGLEMAP

--- index.js
+++ index.js
@@ -1,8 +1,49 @@
 let map;
 
+
+const EXTENT = [-Math.PI * 6378137, Math.PI * 6378137];
+
+function xyzToBounds(x, y, z) {
+    var tileSize = EXTENT[1] * 2 / Math.pow(2, z);
+    var minx = EXTENT[0] + x * tileSize;
+    var maxx = EXTENT[0] + (x + 1) * tileSize;
+    // remember y origin starts at top
+    var miny = EXTENT[1] - (y + 1) * tileSize;
+    var maxy = EXTENT[1] - y * tileSize;
+    return [minx, miny, maxx, maxy];
+}
+
+function getTileURL(coordinates, zoom) {
+    const bbox = xyzToBounds(coordinates.x, coordinates.y, zoom).join(",");
+    console.log("bbox", bbox);
+    const url = "http://172.105.50.132:8080/" +
+        "geoserver/tiger/wms?" +
+        "service=WMS" +
+        "&version=1.1.0" +
+        "&request=GetMap" +
+        "&FORMAT=image%2Fpng" +
+        "&layers=tiger%3Atiger_roads" +
+        "&bbox=" + bbox +
+        "&SRS=EPSG:3857" +
+        "&WIDTH=256&HEIGHT=256" +
+        "&Transparent=True"
+    return url;
+}
+
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
+    const targetDiv = document.getElementById("map")
+    const map = new google.maps.Map(targetDiv, {
+        center: {lat: 40.7850, lng: -73.9660},
+        zoom: 12,
+    });
+
+    const landcover = new google.maps.ImageMapType({
+        getTileUrl: getTileURL,
+        name: "Landcover",
+        alt: "National Land Cover Database 2016",
+        minZoom: 0,
+        maxZoom: 19,
+        opacity: 1.0
+    });
+    map.overlayMapTypes.push(landcover);
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+### WMS service
+
+http://172.105.50.132:8080/geoserver/tiger/wms?service=WMS&version=1.1.0&request=GetMap&layers=tiger%3Atiger_roads&bbox=-74.02722%2C40.684221%2C-73.907005%2C40.878178&width=476&height=768&srs=EPSG%3A4326&styles=&format=application/openlayers
